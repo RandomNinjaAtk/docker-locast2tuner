41e2f0b929e92ce77efaf3fec4765eb8ea1ef062
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-04-09 10:35:36 -0400

    Create run.bash

---
+++ root/config/run.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bash
+if [ -f /config/config ]; then
+	rm  /config/config
+fi
+echo "username=\"${L2DUSER}\"" >> /config/config
+echo "password=\"${L2DPASS}\"" >> /config/config
+echo "override_zipcodes=\"${L2DZIP}\"" >> /config/config
+
+echo "------------------------------------------------------------"
+echo "|~) _ ._  _| _ ._ _ |\ |o._  o _ |~|_|_|"
+echo "|~\(_|| |(_|(_)| | || \||| |_|(_||~| | |<"
+echo "Presenets: locast2dvr"
+echo "------------------------------------------------------------"
+echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
+echo "Project: https://github.com/RandomNinjaAtk/docker-locast2dvr"
+echo "Support: https://discord.gg/JumQXDc"
+echo "------------------------------------------------------------"
+
+locast2dvr --config /config/config --bind-address 0.0.0.0 -ds -m -d 8 -r
